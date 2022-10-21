fe2d84db6ab0468440925c1a17d0b44afdbcf3d2
Die-Gutierrez <dgutierrez@unjbg.edu.pe>
2022-10-21 18:03:13 -0500

    2 distintas formas de declarar un string ya con cadenas de texto

--- string.cpp
+++ string.cpp
@@ -5,12 +5,13 @@ using namespace std;
 
 int main()
 {
-    string cadena;
+    // Se puede ingresar una cadena de caracteres a un string <clase> de 2 distintas formas.
+    string cadena = "Diego Gutierrez";
+    string cadena2 ("Hola mundo");
 
-    cout<<"Ingrese un texto: ";
-    fflush(stdin); // Se usa para limpiar el buffer y evitar problemas al ingresar la cadena de texto.
-    getline(cin, cadena); // Es una funcion, que nos permite mediante 'cin' ingresar elementos o caracteres y 'cadena' es la variable 
-                          // que recibe todo lo que estamos ingresando.
+    cout<<"cadena : "<<cadena<<endl;
+    cout<<"cadena2 : "<<cadena2<<endl;
+    
 
     return 0;
 }
