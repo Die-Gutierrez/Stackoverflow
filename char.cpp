ffcf3f28b4e6fa1d8b229e9e21bbb802ec95616d
Die-Gutierrez <dgutierrez@unjbg.edu.pe>
2022-10-22 09:29:38 -0500

    Usando funciones para string

--- char.cpp
+++ char.cpp
@@ -1,49 +1,23 @@
 #include <iostream>
-#include <string>
+using std::string;
+// Prototipo de funciones
+string aMayuscula(string cadena);
+string aMinuscula(string cadena);
+int main() {
+  string cadena = "Parzibyte.me";
 
-using namespace std;
+  std::cout << "Original: " + cadena;
+  string mayuscula = aMayuscula(cadena);
+  std::cout << "\nMayúscula: " + mayuscula;
+  string minuscula = aMinuscula(cadena);
+  std::cout << "\nMinúscula: " + minuscula;
+}
 
-int main()
-{
-    const char *words[4] = {"uno", "dos", "tres", "cuatro"};
-    string cadenas[4] = {"mundo "};
-    string cad ("Hola");
-
-    cout<<cadenas[0]<<cad<<endl;
-
-    for(int i=0;i<4;i++)
-    {
-        cout<<words[i]<<endl;
-    } 
-    for(int i=0;i<4;i++)
-    {
-        cout<<"Ingrese un texto en ["<<i<<"]: ";
-        fflush(stdin); getline(cin, cadenas[i]);
-    }
-    for(int i=0;i<4;i++)
-    {
-        cout<<cadenas[i]<<endl;
-        cout<<cadenas->size()<<endl;
-    }
-
-    string cadena = "Parzibyte.me";
-
-    std::cout << "Original: " + cadena;
-    // Recorrer toda la cadena
-    for (int i = 0; i < cadena.length(); i++) {
-    // Y cambiar cada letra por su representación
-    // mayúscula
-    cadena[i] = toupper(cadena[i]);
-    }
-    std::cout << "\nMayúscula: " + cadena;
-    // Para convertir a minúscula se usa tolower
-    // Recorrer toda la cadena
-    for (int i = 0; i < cadena.length(); i++) {
-    // Y cambiar cada letra por su representación
-    // minúscula
-    cadena[i] = tolower(cadena[i]);
-    }
-    std::cout << "\nMinúscula: " + cadena;
-
-    return 0;
+string aMayuscula(string cadena) {
+  for (int i = 0; i < cadena.length(); i++) cadena[i] = toupper(cadena[i]);
+  return cadena;
+}
+string aMinuscula(string cadena) {
+  for (int i = 0; i < cadena.length(); i++) cadena[i] = tolower(cadena[i]);
+  return cadena;
 }
